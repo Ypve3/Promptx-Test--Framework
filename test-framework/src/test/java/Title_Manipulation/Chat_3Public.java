755a47bdee122eac255ed312d07bd72f5cb5502c
yash.pardeshi.ve3 <153831181+Ypve3@users.noreply.github.com>
2025-01-14 17:11:50 +0530

    Update Chat_3Public.java

--- test-framework/src/test/java/Title_Manipulation/Chat_3Public.java
+++ test-framework/src/test/java/Title_Manipulation/Chat_3Public.java
@@ -1,7 +1,9 @@
 package Title_Manipulation;
 
 import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,44 +13,52 @@ import org.testng.annotations.Test;
 import genericLib.BaseClass;
 
 public class Chat_3Public extends BaseClass {
-   
-	private WebDriverWait wait;
-	
-	@Test(priority = 1)
-	public void publicChat() {
-		try {
-		 	
-			driver.findElement(By.xpath("(//span[@aria-label='ellipsis'])[1]")).click();
-	     driver.findElement(By.xpath("//span[text()='Public Chat']")).click();
-	     wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	        WebElement notification = wait.until(ExpectedConditions.presenceOfElementLocated(
-	        By.xpath("//div[contains(@class, 'ant-notification-notice')]//div[@class='ant-notification-notice-message']")
-	            ));
-	        
-	        wait.until(ExpectedConditions.textToBePresentInElement(notification, "Successfully Moved to Public Chats"));
 
-	        String actualMessage = notification.getText();
-	        System.out.println("Actual Notification Message: " + actualMessage);
+    private WebDriverWait wait;
 
-	        String expectedMessage = "Successfully Moved to Public Chats";
+    @Test
+    public void publicChat() {
+        try {
+            // Initialize WebDriverWait
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-	        // Assert the notification text
-	        Assert.assertEquals(actualMessage, expectedMessage, "Notification message does not match!");
-	       
-	        driver.findElement(By.xpath("//div[normalize-space()='Public']")).click();
-    		driver.findElement(By.xpath("(//span[@class='anticon anticon-more ant-dropdown-trigger'])[1]")).click();
-    		driver.findElement(By.xpath("//span[text()='Make Chat Private']")).click();
+            // Wait for the ellipsis icon to be clickable and click it
+            WebElement ellipsisIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@aria-label='ellipsis'])[1]")));
+            ellipsisIcon.click();
 
-	    } catch (Exception e) {
-	        System.out.println("Test failed: " + e.getMessage());
-	        Assert.fail("Unexpected exception: " + e.getMessage());
-	    }
-		}
-            
-            
-    		
+            // Wait for 'Public Chat' option to be visible and click it
+            WebElement publicChatOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Public Chat']")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", publicChatOption);
 
-	}
+            // Wait for the notification message
+            WebElement notification = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[contains(@class, 'ant-notification-notice')]//div[@class='ant-notification-notice-message']")
+            ));
+            wait.until(ExpectedConditions.textToBePresentInElement(notification, "Successfully Moved to Public Chats"));
 
-		
-		
+            // Validate the notification message
+            String actualMessage = notification.getText();
+            System.out.println("Actual Notification Message: " + actualMessage);
+
+            String expectedMessage = "Successfully Moved to Public Chats";
+            Assert.assertEquals(actualMessage, expectedMessage, "Notification message does not match!");
+
+            // Navigate to the 'Public' section
+            WebElement publicTab = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[normalize-space()='Public']")));
+            publicTab.click();
+
+            // Click the more options icon for the public chat
+            WebElement moreOptionsIcon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@class='anticon anticon-more ant-dropdown-trigger'])[1]")));
+            moreOptionsIcon.click();
+
+            // Select 'Make Chat Private' option
+            WebElement makePrivateOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Make Chat Private']")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", makePrivateOption);
+
+        } catch (Exception e) {
+            System.out.println("Test failed: " + e.getMessage());
+            Assert.fail("Unexpected exception: " + e.getMessage());
+        }
+    }
+}
+
